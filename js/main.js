ff6b7c983f012ef8062c6f58c0291d3ccc11aeed
Eduarda Velho <email@eduardavelho.com>
2025-09-11 19:30:11 -0300

    hue

--- js/main.js
+++ js/main.js
@@ -3,4 +3,3 @@ import { Screen } from "./screen.js";
 const screenElement = document.querySelector("#screen");
 const screen = new Screen({ element: screenElement, rows: 12, columns: 12 });
 screen.draw();
-screen.blink();
