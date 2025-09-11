43efc3ad3fe0ffee28ec11ba8d7448b4ad20f30c
Eduarda Velho <email@eduardavelho.com>
2025-09-11 20:22:05 -0300

    up

--- js/snake.js
+++ js/snake.js
@@ -19,17 +19,28 @@ export class Snake {
 
   move(direction) {
     const headPosition = structuredClone(this.snake.head.value);
+    let nextHeadPosition = structuredClone(headPosition);
     switch (direction) {
       case "up":
+        nextHeadPosition[0]--;
         break;
       case "down":
+        nextHeadPosition[0]++;
         break;
       case "left":
+        nextHeadPosition[1]--;
         break;
       case "right":
+        nextHeadPosition[1]++;
         break;
       default:
         throw new Error(`The direction "${direction}" is invalid!`);
     }
+
+    const [headRow, headColumn] = headPosition;
+    const [nextHeadRow, nextHeadColumn] = nextHeadPosition;
+    this.snake.head.value = nextHeadPosition;
+    this.board[headRow][headColumn].resetBgColor();
+    this.board[nextHeadRow][nextHeadColumn].setBgColor("red");
   }
 }
