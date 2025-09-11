dc39451b74f2ef70b520cacdb5655965dd715f3a
Eduarda Velho <email@eduardavelho.com>
2025-09-11 20:10:37 -0300

    up

--- js/snake.js
+++ js/snake.js
@@ -2,6 +2,7 @@ import { LinkedList } from "./linked-list/linked-list.js";
 
 export class Snake {
   constructor({ board, headPosition = [0, 0] }) {
+    this.board = board;
     this.snake = new LinkedList();
     this.snake.add(headPosition);
   }
@@ -11,7 +12,24 @@ export class Snake {
     while (currentNode !== null) {
       const [row, column] = currentNode.value;
       console.log(row, column);
+      this.board[row][column].setBgColor("red");
       currentNode = currentNode.next;
+    }
+  }
+
+  move(direction) {
+    const headPosition = structuredClone(this.snake.head.value);
+    switch (direction) {
+      case "up":
+        break;
+      case "down":
+        break;
+      case "left":
+        break;
+      case "right":
+        break;
+      default:
+        throw new Error(`The direction "${direction}" is invalid!`);
     }
   }
 }
