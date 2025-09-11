0b665493eed7ad1d2157883d23da42c092f6fbbd
Eduarda Velho <email@eduardavelho.com>
2025-09-11 19:47:20 -0300

    up

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 import { Screen } from "./screen.js";
+import { Snake } from "./snake.js";
 
 const screenElement = document.querySelector("#screen");
 const screen = new Screen({ element: screenElement, rows: 12, columns: 12 });
+const snake = new Snake({ board: screen.board });
 screen.draw();
+snake.draw();
--- js/snake.js
+++ js/snake.js
@@ -1,1 +1,17 @@
-export class Snake {}
+import { LinkedList } from "./linked-list/linked-list.js";
+
+export class Snake {
+  constructor({ board, headPosition = [0, 0] }) {
+    this.snake = new LinkedList();
+    this.snake.add(headPosition);
+  }
+
+  draw() {
+    let currentNode = this.snake.head;
+    while (currentNode !== null) {
+      const [row, column] = currentNode.value;
+      console.log(row, column);
+      currentNode = currentNode.next;
+    }
+  }
+}
